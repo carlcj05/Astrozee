ace9c7b4b10ae9c77993d4a43f32329d6f9c0382
carl <carlcj05@gmail.com>
2026-01-07 16:24:18 +0100

    test

--- Core/models/PlanetPosition.swift
+++ Core/models/PlanetPosition.swift
@@ -1,4 +1,3 @@
-
 import Foundation
 
 struct PlanetPosition: Identifiable, Codable {
@@ -9,6 +8,10 @@ struct PlanetPosition: Identifiable, Codable {
     let sign: String
 
     var degreeInSign: String {
-        formatDegreeInSign(longitude)
+        var lon = longitude.truncatingRemainder(dividingBy: 360)
+        if lon < 0 { lon += 360 }
+        let deg = Int(lon) % 30
+        let min = Int((lon - floor(lon)) * 60.0)
+        return String(format: "%02d°%02d'", deg, min)
     }
 }
